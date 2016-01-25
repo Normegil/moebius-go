ffaddaf83016a13261a59cd170f4bf61510f4ec6
Olivier Ory <mail@normegil.be>
2016-01-25 10:37:21 +0100

    Maintenance: Add documentation and format existing code

--- app.go
+++ app.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-  "fmt"
-  "github.com/normegil/moebius-go/connector"
+	"fmt"
+	"github.com/normegil/moebius-go/connector"
 )
 
 func main() {
-  mangas, err := connector.LoadMangas("en")
-  if nil != err {
-    panic(err)
-  }
-  for _, manga := range mangas {
-    fmt.Println(manga.Title)
-  }
+	mangas, err := connector.LoadMangas("en")
+	if nil != err {
+		panic(err)
+	}
+	for _, manga := range mangas {
+		fmt.Println(manga.Title)
+	}
 }
--- connector/mangaeden/mangaEden.go
+++ connector/mangaeden/mangaEden.go
@@ -1,67 +1,77 @@
+// Package mangaeden implements the MangaEden API (http://www.mangaeden.com/api/).
 package mangaeden
 
 import (
-  "encoding/json"
-  "errors"
-  "fmt"
-  "io/ioutil"
-  "net/http"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
+// MangaAPI is the structure representing the response from MangaEden when loading the manga list.
+// It represents only the list of mangas
 type MangaAPI struct {
-  I string `json: i`
-  T string `json: t`
-  C []string `json: c`
-  Im string `json: im`
-  A string `json: a`
-  S int `json: s`
+	I  string   // ID
+	T  string   // Title
+	C  []string // Tags
+	Im string   // Main image
+	A  string   // Alias
+	/* Status:
+	   -0: Suspended
+	   -1: Ongoing
+	   -2: Completed
+	*/
+	S int
 }
 
 type mangaResponse struct {
-  Manga []MangaAPI
+	Manga []MangaAPI
 }
 
+// LoadMangas is loading the manga list from MangaEden.
+// It accept a language parameter which should be "en"(English) or "it"(Italian). If the string is empty, it will default to english.
 func LoadMangas(language string) ([]MangaAPI, error) {
-  languageCode, err := getLanguageCode(language)
-  if nil != err {
-    return nil, err
-  }
+	languageCode, err := getLanguageCode(language)
+	if nil != err {
+		return nil, err
+	}
 
-  const mangaURL = "https://www.mangaeden.com/api/list/%d/"
-  url := fmt.Sprintf(mangaURL, languageCode)
-  resp, err := http.Get(url)
-  if nil != err {
-    return nil, err
-  }
-  defer resp.Body.Close()
+	const mangaURL = "https://www.mangaeden.com/api/list/%d/"
+	url := fmt.Sprintf(mangaURL, languageCode)
+	resp, err := http.Get(url)
+	if nil != err {
+		return nil, err
+	}
+	defer resp.Body.Close()
 
-  mangaResp, err := parseResponse(resp)
-  if nil != err {
-    return nil, err
-  }
-  return mangaResp.Manga, nil
+	mangaResp, err := parseResponse(resp)
+	if nil != err {
+		return nil, err
+	}
+	return mangaResp.Manga, nil
 }
 
 func getLanguageCode(language string) (int, error) {
-  if "" == language || "en" == language {
-    return 0, nil
-  }
-  if "it" == language {
-    return 1, nil
-  }
-  return -1, errors.New("Language not supported: " + language)
+	if "" == language || "en" == language {
+		return 0, nil
+	}
+	if "it" == language {
+		return 1, nil
+	}
+	return -1, errors.New("Language not supported: " + language)
 }
 
 func parseResponse(resp *http.Response) (mangaResponse, error) {
-  content, err := ioutil.ReadAll(resp.Body)
-  if nil != err {
-    return mangaResponse{}, err
-  }
+	content, err := ioutil.ReadAll(resp.Body)
+	if nil != err {
+		return mangaResponse{}, err
+	}
 
-  var mangaResp mangaResponse
-  err = json.Unmarshal(content, &mangaResp)
-  if nil != err {
-    return mangaResponse{}, err
-  }
-  return mangaResp, nil
+	var mangaResp mangaResponse
+	err = json.Unmarshal(content, &mangaResp)
+	if nil != err {
+		return mangaResponse{}, err
+	}
+	return mangaResp, nil
 }
